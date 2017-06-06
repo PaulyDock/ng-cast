9a64154c9ae5ceaf59aba03034335bae5a239b29
Hack Reactor Student <student@hackreactor.com>
2017-06-06 15:56:18 -0700

    video player renders

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ angular.module('video-player')
 .component('app', {
   controller: function() {
     this.videos = window.exampleVideoData;
+    this.currentVideo = this.videos[0];
   },
   templateUrl: 'src/templates/app.html'
 });
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,20 @@
 angular.module('video-player')
 
 .component('videoPlayer', {
+  controller: function() {
+    console.log('videoPlayer this', this);
+    console.log('this.video', this.video);
+  },
+  bindings: {
+    video: '<'
+    // currentTitle: '<',
+    // currentDescription: '<',
+    // currentImage: '<',
+  },
+  controller: function() {
+    this.videoUrl = () => {
+      return `https://www.youtube.com/embed/${this.video.id.videoId}`; //ternary oper. ??
+    };
+  },
   templateUrl: 'src/templates/videoPlayer.html'
 });
