c095b77a4e66d99dc0fc620c7065f6dbed7eed43
Hack Reactor Student <student@hackreactor.com>
2017-06-05 18:14:27 -0700

    risky commit pre-altering videoList template

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,7 @@
 angular.module('video-player')
+.controller('videoCtrlInit', ['$scope', function($scope) {
+  $scope.videos = window.exampleVideoData;
+}])
 
 .component('app', {
   templateUrl: 'src/templates/app.html'
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,7 +1,5 @@
 angular.module('video-player')
-.controller('videoListController', function($scope) {
-  $scope.videos = window.exampleVideoData;
-})
+
 .component('videoList', {
   templateUrl: 'src/templates/videoList.html'
 });
